4525bc90f4c85f3bd851d1d8c6cfd738392c88b2
Debugon <games0421@gmail.com>
2016-12-06 02:54:03 +0900

    SoundEffect.cs、音源追加

---
+++ ChewingGum/ChewingGum/SoundEffect.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace ChewingGum
+{
+    class SoundEffect
+    {
+        #region フィールド
+
+        private SoundEffect itemSelect;
+        private SoundEffect itemSelected;
+        private SoundEffect itemCanceled;
+
+        #endregion
+    }
+}
